ec90587af8dba0cb2bf153308df0f8b4055121a3
alake16 <aclake@umich.edu>
2019-12-02 19:54:45 -0600

    Getting more there:

--- sensor.c
+++ sensor.c
@@ -24,6 +24,44 @@ void loop(int s)
   delay(20); //Delay so we don't overload the serial port
 }
 
+int pulseIn(int pin, int level, int timeout)
+{
+   struct timeval tn, t0, t1;
+
+   long micros;
+
+   gettimeofday(&t0, NULL);
+
+   micros = 0;
+
+   while (digitalRead(pin) != level)
+   {
+      gettimeofday(&tn, NULL);
+
+      if (tn.tv_sec > t0.tv_sec) micros = 1000000L; else micros = 0;
+      micros += (tn.tv_usec - t0.tv_usec);
+
+      if (micros > timeout) return 0;
+   }
+
+   gettimeofday(&t1, NULL);
+
+   while (digitalRead(pin) == level)
+   {
+      gettimeofday(&tn, NULL);
+
+      if (tn.tv_sec > t0.tv_sec) micros = 1000000L; else micros = 0;
+      micros = micros + (tn.tv_usec - t0.tv_usec);
+
+      if (micros > timeout) return 0;
+   }
+
+   if (tn.tv_sec > t1.tv_sec) micros = 1000000L; else micros = 0;
+   micros = micros + (tn.tv_usec - t1.tv_usec);
+
+   return micros;
+}
+
 int main(int argc, char *argv[]) {
     int s = setup();
     loop(s);
