451895033e89bb4ba31208e206900adf2482375d
Andrew Lake <alake1@luc.edu>
2019-12-17 18:50:51 -0600

    Threading with three sensors

--- sonarTest.cpp
+++ sonarTest.cpp
@@ -18,12 +18,16 @@ void *function(void* voidSonar) {
 			if (sonar -> getPosition() == 0) {
 				cout << "LEFT";
 			}
-			else {
+			else if (sonar -> getPosition() == 1) {
 				cout << "RIGHT";
+			}
+			else {
+				cout << "BACK RIGHT";
 			}
 			cout << "CLOSE" << endl;
 		}
 	}
+	cout << "Exiting Thread " << sonar -> getPosition() << endl;
 	pthread_exit(NULL);	
 }
 
@@ -31,13 +35,17 @@ int main(int argc, char *argv[]) {
 	
 	pthread_t leftThread;
 	pthread_t rightThread;
+	pthread_t backRightThread;
 
     if (wiringPiSetupGpio() == -1) return -1;
     
     Sonar left(13, 6, 0);
     Sonar right(26, 19, 1);
+    Sonar backRight(24, 23, 2);
+    
     pthread_create(&leftThread, NULL, function, &left);
     pthread_create(&rightThread, NULL, function, &right);
+    pthread_create(&backRightThread, NULL, function, &backRight);
     pthread_exit(NULL);
     
 	return 0;
